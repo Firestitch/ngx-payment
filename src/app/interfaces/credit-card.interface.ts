8bb4c40f3d54293c23a3aec9557ba596381f1f0b
Anatoly <mendor1990@gmail.com>
2019-08-16 11:59:39 +0300

    update credit card interface

--- src/app/interfaces/credit-card.interface.ts
+++ src/app/interfaces/credit-card.interface.ts
@@ -1,8 +1,10 @@
+// NOTE: fields which has '0' as a first number can't be integer. Any numeric types of variables
+// automatically removing first zero digits
 export interface CreditCard {
   name?: string;
   number?: number;
-  expiry_month?: number;
-  expiry_year?: number;
+  expiry_month?: string;
+  expiry_year?: string;
   cvv?: number;
   type?: string;
 }
