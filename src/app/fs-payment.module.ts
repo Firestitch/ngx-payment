ba00b32c813a5db5459bb3e27631d559248123dc
Eugene Kubesh <codbasters@gmail.com>
2021-01-12 23:23:15 +0300

    update material imports

--- src/app/fs-payment.module.ts
+++ src/app/fs-payment.module.ts
@@ -1,13 +1,12 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import {
-  MatFormFieldModule,
-  MatSelectModule,
-  MatInputModule,
-  MatIconModule
-} from '@angular/material';
 import { FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatIconModule } from '@angular/material/icon'
+
 import { FsAddressModule } from '@firestitch/address';
 import { FsFormModule } from '@firestitch/form';
 import { FsLabelModule } from '@firestitch/label';
